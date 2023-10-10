2463b6a7bd00a5064c08fb136456a0d13b18411e
Aaron McClellan <mcclellana4@udayton.edu>
2023-10-09 20:25:01 -0400

    Installed mongoose

--- application/index.js
+++ application/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.use('/css', express.static('css'));
+
 app.get('/', (req, res) => {
     res.send("Test");
 });
